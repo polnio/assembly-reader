a3550ddc0c093f27e180694c8904c64f8ec56b77
polnio <polniolino@gmail.com>
2022-05-25 15:25:40 +0200

    add halt, branches, b , and b<cond> operations

--- dist/main.js
+++ dist/main.js
@@ -6,7 +6,9 @@ exports.__esModule = true;
 var fs_1 = __importDefault(require("fs"));
 var process_1 = require("process");
 var registers = new Array(8);
-var noline = 1;
+var branches = {};
+var noline;
+var result;
 function logError(errorText) {
     console.error("Error l.".concat(noline, ": ").concat(errorText));
     (0, process_1.exit)(1);
@@ -35,6 +37,15 @@ function getOperand(operand) {
         return getRegister(operand);
     logError("Invalid operand: ".concat(operand));
 }
+function goToBranch(branch, condition) {
+    if (condition === void 0) { condition = true; }
+    if (condition) {
+        var nolineBranch = branches[branch];
+        if (nolineBranch === undefined)
+            logError("Branch ".concat(branch, " not found"));
+        noline = nolineBranch;
+    }
+}
 function anyToNumber(thing) {
     if (typeof thing === 'number')
         return thing;
@@ -43,7 +54,14 @@ function anyToNumber(thing) {
     return NaN;
 }
 var commands = {
-    out: function (text) { console.log(getOperand(text)); },
+    out: function () {
+        var texts = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            texts[_i] = arguments[_i];
+        }
+        console.log.apply(console, texts.map(getOperand));
+    },
+    halt: function () { (0, process_1.exit)(); },
     mov: function (output, input) { setRegister(output, getOperand(input)); },
     add: function (output, arg1, arg2) {
         var number1 = anyToNumber(getOperand(arg1));
@@ -54,30 +72,55 @@ var commands = {
         var number1 = anyToNumber(getOperand(arg1));
         var number2 = anyToNumber(getOperand(arg2));
         setRegister(output, number1 - number2);
-    }
+    },
+    cmp: function (arg1, arg2) {
+        var result1 = getOperand(arg1);
+        var result2 = getOperand(arg2);
+        result = result1 < result2
+            ? 'less'
+            : result1 > result2
+                ? 'greater'
+                : 'equal';
+    },
+    b: function (label) { goToBranch(label); },
+    beq: function (label) { goToBranch(label, result === 'equal'); },
+    bne: function (label) { goToBranch(label, result !== 'equal'); },
+    bgt: function (label) { goToBranch(label, result === 'greater'); },
+    blt: function (label) { goToBranch(label, result === 'less'); }
 };
 fs_1["default"].readFile('test/test.txt', function (err, data) {
+    var _a, _b;
     if (err)
         throw err;
     var lines = data.toString().split('\n');
-    while (noline <= lines.length) {
-        var line = lines[noline - 1];
-        if (line.trim() === '') {
-            noline++;
+    for (var i in lines) {
+        var line = lines[i].trim();
+        if (line.endsWith(':')) {
+            branches[line.substring(0, line.length - 1)] = parseInt(i);
             continue;
         }
+    }
+    noline = 0;
+    while (noline < lines.length) {
+        noline++;
+        var line = lines[noline - 1].trim();
+        if (line === '')
+            continue;
+        if (line.startsWith(';'))
+            continue;
+        if (line.endsWith(':'))
+            continue;
         var found = false;
         for (var j in commands) {
-            var match = new RegExp("^".concat(j, "(?: +(.*))?"), 'i').exec(line);
+            var match = new RegExp("^".concat(j, "(?: +(.*))?$"), 'i').exec(line);
             if (match) {
-                var args = match[1].split(',').map(function (arg) { return arg.trim(); });
-                commands[j].apply(commands, args);
+                var args = (_b = (_a = match[1]) === null || _a === void 0 ? void 0 : _a.split(',')) === null || _b === void 0 ? void 0 : _b.map(function (arg) { return arg.trim(); });
+                args ? commands[j].apply(commands, args) : commands[j]();
                 found = true;
                 break;
             }
         }
         if (!found)
             logError('Unknown command');
-        noline++;
     }
 });
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import fs from 'fs'
 import { exit } from 'process'
 
 const registers = new Array(8)
-let noline = 0
+const branches: {[key: string]: number} = {}
+let noline: number
+let result: 'equal' | 'greater' | 'less'
 
 function logError (errorText: string) {
     console.error(`Error l.${noline}: ${errorText}`)
@@ -33,6 +35,14 @@ function getOperand (operand: string) {
     logError(`Invalid operand: ${operand}`)
 }
 
+function goToBranch (branch: string, condition = true) {
+    if (condition) {
+        const nolineBranch = branches[branch]
+        if (nolineBranch === undefined) logError(`Branch ${branch} not found`)
+        noline = nolineBranch
+    }
+}
+
 function anyToNumber (thing: any) {
     if (typeof thing === 'number') return thing
     if (typeof thing === 'string') return parseInt(thing)
@@ -40,7 +50,8 @@ function anyToNumber (thing: any) {
 }
 
 const commands = {
-    out: (text: any) => { console.log(getOperand(text)) },
+    out: (...texts: string[]) => { console.log(...texts.map(getOperand)) },
+    halt: () => { exit() },
     mov: (output: string, input: string) => { setRegister(output, getOperand(input)) },
     add: (output: string, arg1: string, arg2: string) => {
         const number1 = anyToNumber(getOperand(arg1))
@@ -51,25 +62,49 @@ const commands = {
         const number1 = anyToNumber(getOperand(arg1))
         const number2 = anyToNumber(getOperand(arg2))
         setRegister(output, number1 - number2)
-    }
+    },
+    cmp: (arg1: string, arg2: string) => {
+        const result1 = getOperand(arg1)
+        const result2 = getOperand(arg2)
+        result = result1 < result2
+            ? 'less'
+            : result1 > result2
+                ? 'greater'
+                : 'equal'
+    },
+    b: (label: string) => { goToBranch(label) },
+    beq: (label: string) => { goToBranch(label, result === 'equal') },
+    bne: (label: string) => { goToBranch(label, result !== 'equal') },
+    bgt: (label: string) => { goToBranch(label, result === 'greater') },
+    blt: (label: string) => { goToBranch(label, result === 'less') }
 }
 
 fs.readFile('test/test.txt', (err, data) => {
     if (err) throw err
 
     const lines = data.toString().split('\n')
+    for (const i in lines) {
+        const line = lines[i].trim()
+        if (line.endsWith(':')) {
+            branches[line.substring(0, line.length - 1)] = parseInt(i)
+            continue
+        }
+    }
 
+    noline = 0
     while (noline < lines.length) {
         noline++
-        const line = lines[noline - 1]
-        if (line.trim() === '') continue
+        const line = lines[noline - 1].trim()
+        if (line === '') continue
+        if (line.startsWith(';')) continue
+        if (line.endsWith(':')) continue
 
         let found = false
         for (const j in commands) {
-            const match = new RegExp(`^${j}(?: +(.*))?`, 'i').exec(line)
+            const match = new RegExp(`^${j}(?: +(.*))?$`, 'i').exec(line)
             if (match) {
-                const args = match[1].split(',').map(arg => arg.trim())
-                commands[j](...args)
+                const args = match[1]?.split(',')?.map(arg => arg.trim())
+                args ? commands[j](...args) : commands[j]()
                 found = true
                 break
             }
